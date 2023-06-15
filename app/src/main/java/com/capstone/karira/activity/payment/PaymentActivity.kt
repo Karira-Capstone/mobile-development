52e497b4f2901db0662009fcbf782b025e366c2b
rysujana <ryansujanahusein@gmail.com>
2023-06-15 12:08:13 +0700

    feat: add notification, transaction, payment feature

---
+++ app/src/main/java/com/capstone/karira/activity/payment/PaymentActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.karira.activity.payment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.karira.R
+
+class PaymentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment)
+    }
+}
